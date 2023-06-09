ec51cdecc4790e1b1d6405a6081816d2a81b136a
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-10 00:32:04 +0600

    delete selected class

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjpsvw7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -65,6 +65,12 @@ async function run() {
       console.log(item);
       const result =await sclassesCollection.insertOne(item);
       res.send(result);
+    })
+    app.delete('/sclasses/:id', async(req,res)=>{
+        const id = req.params.id
+        const query ={_id: new ObjectId(id)};
+        const result =await sclassesCollection.deleteOne(query);
+        res.send(result);
     })
 
 
