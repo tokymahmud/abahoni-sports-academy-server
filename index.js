8b7d2b94a07f9736b0b9c4d833540de9bc1a9ef9
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-10 20:27:43 +0600

    instructor admin role done

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run() {
     const instructorsCollection =client.db('Abahoni').collection('instructors');
     const sclassesCollection =client.db('Abahoni').collection('sclasses');
 
+
+    app.get('/users', async(req,res)=>{
+        const result =await usersCollection.find().toArray();
+        res.send(result);
+    })
+
     app.post('/users', async(req,res)=>{
         const user=req.body;
         const query ={email:user.email}
@@ -41,6 +47,29 @@ async function run() {
         const result =await usersCollection.insertOne(user);
         res.send(result);
 
+    })
+
+    app.patch('/users/admin/:id', async(req,res)=>{
+        const id =req.params.id;
+        const filter ={_id: new ObjectId(id)};
+        const updateDoc ={
+            $set:{
+                role:'admin'
+            },
+        };
+        const result =await usersCollection.updateOne(filter,updateDoc);
+        res.send(result);
+    })
+    app.patch('/users/instructor/:id', async(req,res)=>{
+        const id =req.params.id;
+        const filter ={_id: new ObjectId(id)};
+        const updateDoc ={
+            $set:{
+                role:'instructor'
+            },
+        };
+        const result =await usersCollection.updateOne(filter,updateDoc);
+        res.send(result);
     })
 
     app.get('/classes', async(req,res)=>{
