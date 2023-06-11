dd6957db7f603428383ef8ac373585563de4f5d0
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-11 18:12:45 +0600

    instructor role added

--- index.js
+++ index.js
@@ -56,8 +56,29 @@ async function run() {
         res.send({token})
     })
 
+    const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        if (user?.role !== 'admin') {
+          return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
+        next();
+      }
+  
+    const verifyInstructor = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        if (user?.role !== 'instructor') {
+          return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
+        next();
+      }
+  
 
-    app.get('/users', verifyJWT, async(req,res)=>{
+
+    app.get('/users', verifyJWT,verifyAdmin, async(req,res)=>{
         const result =await usersCollection.find().toArray();
         res.send(result);
     })
@@ -101,6 +122,19 @@ async function run() {
         const result = await usersCollection.updateOne(filter, updateDoc);
         res.send(result);
   
+      })
+
+      app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+        const email = req.params.email;
+  
+        if (req.decoded.email !== email) {
+          res.send({ instructor: false })
+        }
+  
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        const result = { instructor: user?.role === 'instructor' }
+        res.send(result);
       })
   
     app.patch('/users/instructor/:id', async(req,res)=>{
