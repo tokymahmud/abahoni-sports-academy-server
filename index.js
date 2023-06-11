83406c9db89eb7fc1adccbf281652907607f0cd8
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-11 13:54:19 +0600

    fetching issue resolved and  authorized

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 const verifyJWT= (req,res,next)=>{
-    const authorization =req.headders.authorization;
+    const authorization =req.headers.authorization;
     if(!authorization){
-        return res.send.status(401).send({error:true, message:'unauthorized access'});
+        return res.status(401).send({error:true, message:'unauthorized access'});
 
     }
-    const token=authorization.split('')[1];
+    const token=authorization.split(' ')[1];
     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
         if(err){
-            return res.send.status(401).send({error:true, message:'unauthorized access'});
+            return res.status(401).send({error:true, message:'unauthorized access'});
         }
         req.decoded=decoded;
         next();
@@ -52,12 +52,12 @@ async function run() {
 
     app.post('/jwt',(req,res)=>{
         const user=req.body;
-        const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'10d'})
         res.send({token})
     })
 
 
-    app.get('/users', async(req,res)=>{
+    app.get('/users', verifyJWT, async(req,res)=>{
         const result =await usersCollection.find().toArray();
         res.send(result);
     })
@@ -73,18 +73,36 @@ async function run() {
         res.send(result);
 
     })
+    // ,verifyJWT
 
-    app.patch('/users/admin/:id', async(req,res)=>{
-        const id =req.params.id;
-        const filter ={_id: new ObjectId(id)};
-        const updateDoc ={
-            $set:{
-                role:'admin'
-            },
-        };
-        const result =await usersCollection.updateOne(filter,updateDoc);
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+        const email = req.params.email;
+  
+        if (req.decoded.email !== email) {
+          res.send({ admin: false })
+        }
+  
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' }
         res.send(result);
-    })
+      })
+
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+  
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+  
+      })
+  
     app.patch('/users/instructor/:id', async(req,res)=>{
         const id =req.params.id;
         const filter ={_id: new ObjectId(id)};
@@ -106,31 +124,48 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/sclasses',verifyJWT, async(req,res)=>{
-        const email= req.query.email;
-        console.log(email);
-        if(!email){
-            res.send([]);
-        }
-        else {
-            try {
-                // things new added.need to check todo
-                const decodedEmail=req.decoded.email;
-                if(email!==decodedEmail){
-                    return res.status(403).send({error:true, message:'forbidded access'})
-                }
-              const query = { email: email };
-              const result = await sclassesCollection.find(query).toArray();
-              res.send(result);
-            } catch (error) {
-              console.error(error);
-            }
-          }
-        // const query ={email:email};
-        // const result =await sclassesCollection.find(query).toArray();
-        // res.send(result);
+    // app.get('/sclasses', async(req,res)=>{
+    //     const email= req.query.email;
+    //     console.log(email);
+    //     if(!email){
+    //         res.send([]);
+    //     }
+    //     else {
+    //         try {
+    //             // things new added.need to check todo
+    //             const decodedEmail=req.decoded.email;
+    //             if(email!==decodedEmail){
+    //                 return res.status(403).send({error:true, message:'forbidded access'})
+    //             }
+    //           const query = { email: email };
+    //           const result = await sclassesCollection.find(query).toArray();
+    //           res.send(result);
+    //         } catch (error) {
+    //           console.error(error);
+    //         }
+    //       }
+    //     // const query ={email:email};
+    //     // const result =await sclassesCollection.find(query).toArray();
+    //     // res.send(result);
 
-    });
+    // });
+
+    app.get('/sclasses', verifyJWT, async (req, res) => {
+        const email = req.query.email;
+  
+        if (!email) {
+          res.send([]);
+        }
+  
+        const decodedEmail = req.decoded.email;
+        if (email !== decodedEmail) {
+          return res.status(403).send({ error: true, message: 'forbidden access' })
+        }
+  
+        const query = { email: email };
+        const result = await sclassesCollection.find(query).toArray();
+        res.send(result);
+      });
 
     app.post('/sclasses',async(req,res)=>{
       const item=req.body;
