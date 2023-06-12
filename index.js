48e3df0c36d5bada6ae2870d4b0ca352163516f0
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-12 14:08:46 +0600

    vercel deployment

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect(); 
+    // await client.connect(); 
     const classesCollection =client.db('Abahoni').collection('classes');
     const usersCollection =client.db('Abahoni').collection('users');
     const instructorsCollection =client.db('Abahoni').collection('instructors');
@@ -296,7 +296,7 @@ async function run() {
         res.send({ insertResult, deleteResult });
       })
        
-      
+
       app.get('/payments', async (req, res) => {
         try {
           const result = await paymentCollection.find().toArray();
@@ -308,7 +308,7 @@ async function run() {
       });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
